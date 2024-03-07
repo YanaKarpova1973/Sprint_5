26a65e6ac12b6d8c2cd41c22f15d4e2f519bd223
YanaKarpova1973 <160159043+YanaKarpova1973@users.noreply.github.com>
2024-03-07 23:38:18 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,1 @@
-import pytest
 
-@pytest.fixture(scope='function')
-def correct_user_info():
-    return {'name': 'Яна', 'login': 'karpova_6125@yandex.ru', 'password': '1234567'}
-
-@pytest.fixture(scope='function')
-def incorrect_user_info():
-    return {'name': '', 'login': 'karpova_yandex_ru', 'password': '12345'}
